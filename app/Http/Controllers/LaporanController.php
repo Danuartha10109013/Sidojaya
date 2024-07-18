930c61153d8cc5b81576378697523cc5af87d44a
danuartha <danuartha.10109013@student.polsub.ac.id>
2024-07-18 23:31:21 +0700

    tiket & maps sementara

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\M_Aboutus;
+use App\Models\M_kategori;
 use Illuminate\Http\Request;
 use App\Models\M_tiket;
 use App\Models\M_wisata;
@@ -35,10 +37,23 @@ class LaporanController extends Controller
 
     public function history()
     {
+        $data_aboutus = M_Aboutus::all();
+        $data_kategori = M_kategori::all();
+        $data_wisata = M_wisata::all();
+
         $id = Auth::user()->id;
-        $data_order = Order::with('wisata')->where('id_user', $id)->paginate(10);
+
+        // Retrieve paginated orders for the authenticated user
+        $data_order = Order::where('id_user', $id)->paginate(10);
+        
+        // Retrieve ids of 'id_wisata' from the orders of the authenticated user
+        $same = Order::where('id_user', $id)->pluck('id_wisata');
+        
+        // Retrieve 'M_wisata' records where 'id_wisata' matches the collected ids
+        $wisata = M_wisata::whereIn('id', $same)->get();
+        
     
-        return view('historypengunjung', compact('data_order'));
+        return view('historypengunjung', compact('data_order','wisata','data_aboutus','data_kategori','data_wisata'));
     }
 
     public function pemesanan(){
